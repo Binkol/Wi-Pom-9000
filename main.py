104975d922bfe6b7cae02fe03587fbe0a728f474
Tomasz Michalik <t.michalik630@gmail.com>
2022-06-06 21:15:52 +0200

    final working regulator (chyba nie rozproszony)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 from bokeh.plotting import figure, show
+from progress.bar import Bar
 
 #TODO zmniejszyć okres próbkowania #mamy
 #TODO napisac funkcje w Termostacie, która przyjmuje sygnał sterujący jako parametr do sterowania procentem hot/cold
@@ -19,12 +20,14 @@ N = int(t_sim/Tp)
 
 kp = 0.15 #wzmocnienie regulatora
 Ti = 7.5 #czas zdwojenia
-Td = 0.0
+Td = 0.2
 
-termo = Termostat(60, 10, 15, 0.6) #(hot, cold, flow_spped, hot_percentage)
-bath = Bath(temp_0, 0.10, 300)       #(start_water_temp, start_water_amount, smax_water_amount)
+termo = Termostat(60, 10, 5, 0.6) #(hot, cold, flow_spped, hot_percentage)
+bath = Bath(temp_0, 10, 400)       #(start_water_temp, start_water_amount, smax_water_amount)
 counter = 0
 
+u_min = 0
+u_max = 10
 u_PID = [0.5, ]
 bath_temps = []
 alpha = [0.5, ]
@@ -32,29 +35,28 @@ e = [0.0, ]
 
 def temp_pid():
     global u_PID
+    bar = Bar('Processing', max=N-1)
     for i in range(N - 1):
         e.append(temp_target - bath.current_water_temp)
         u_PID.append(kp * (e[i] + Tp * sum(e) / Ti + Td * (e[i] - e[i-1]) / Tp))
         
-        #print(u_PID[-1])
-
-        if u_PID[-1] >= 3.0:
+        if u_PID[-1] < 10 and u_PID[-1] >= 0:
+            termo.change_hot_percentage(u_PID[-1]/u_max)
+        elif u_PID[-1] > 10:
             termo.change_hot_percentage(1.0)
-        elif u_PID[-1] <= 0:
-            termo.change_hot_percentage(0)
         else:
-            termo.change_hot_percentage(u_PID[-1])
+            termo.change_hot_percentage(u_min)
 
         termo.update_water_temp()
         bath.mix_waters(Tp, termo.flow_speed, termo.mixed_water_temp)
-        bath.update_water_amount(termo.flow_speed)
-
+        bath.update_water_amount(Tp, termo.flow_speed)
 
         bath_temps.append(bath.current_water_temp)
-        #print("e: {}, temp_target: {} u_PID: {}, temp_t: {}".format(e[-1], temp_target, u_PID[i], termo.mixed_water_temp))
-
-    #print("e:",e[1:10])
-    #print("u_PID:",u_PID[1:10])
+        if bath.is_overflow():
+            print("\nWater Overflow. Call the cops!")
+            return
+        bar.next()
+    bar.finish()
 
 def plotting1():
     p = figure(title="An example of PID", x_axis_label='n', y_axis_label='u_pid')
@@ -67,7 +69,6 @@ def plotting2():
     show(p)
 
 
-#print("It took {} seconds".format(counter))
 temp_pid()
 plotting1()
 plotting2()
